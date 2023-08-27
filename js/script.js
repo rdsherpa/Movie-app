fc00163026b9a5be7b2475adf79db29235d66640
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-08-26 18:18:09 -0600

    commits made so far

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+const global = {
+  currentPage: window.location.pathname,
+};
+
+async function displayPopularMovies() {
+  const results = await fetchAPIData('movie/popular');
+  console.log(results);
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '711af147f291a70353af78f4391c1d51'
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      displayPopularMovies();
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case 'tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
